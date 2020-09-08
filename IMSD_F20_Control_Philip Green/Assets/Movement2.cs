d8661a403a442b7713463b70d3a5085540ef6daf
Philip Green <Philip.Green@mail.citytech.cuny.edu>
2020-09-07 20:15:52 -0400

    Added more scripts for shapes.

---
+++ IMSD_F20_Control_Philip Green/Assets/Movement2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour
+{
+    public Transform myTransform;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform.position = new Vector3(-10, myTransform.position.y, 0);
+        //timer = 0;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position += new Vector3(1, 0, 0); 
+    }
+}
