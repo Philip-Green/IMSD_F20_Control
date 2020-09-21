92c4970be671b342d6ebdb7f5b0406d6845a16ce
Philip Green <Philip.Green@mail.citytech.cuny.edu>
2020-09-21 19:50:03 -0400

    Added new elements

---
+++ IMSD_F20_Control_Philip Green/Assets/Frogger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frogger : MonoBehaviour
+{
+    public Transform myTransform;
+    private float speed = 9.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) //Hold key to be true.
+        {
+            myTransform.position += new Vector3(0, Time.deltaTime * speed, 0);
+        }
+
+
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            myTransform.position += Vector3.up;
+        }
+    
+        if (Input.GetKey(KeyCode.S))
+        {
+            myTransform.position += new Vector3(0, -Time.deltaTime * speed, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            myTransform.position += Vector3.down;
+        }
+    }   
+}
---
+++ IMSD_F20_Control_Philip Green/Assets/Trying.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trying : MonoBehaviour
+{
+    public Transform myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            myTransform.position += Vector3.left;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            myTransform.position += Vector3.right;
+        }
+    }   
+}
